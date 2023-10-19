9835dcdbe8722251c47eb75bcb456ed939ba2098
leo <townhelpemail@gmail.com>
2023-10-18 21:27:43 -0400

    ⚙️ fix: change button.
    Changed the button from "Invite" to "Source."p

--- components/Static/Header.jsx
+++ components/Static/Header.jsx
@@ -166,9 +166,9 @@ const Header = ({ $, NavItems }) => {
                                 </Menu.Items>
                             </Transition>
                         </Menu>
-                        <Link href="https://discord.gg/sshkAMJjcA">
+                        <Link href="https://github.com/StylarBot/Stylar-TS">
                             <a className="w-auto flex items-center justify-center shadow-lg gap-x-2 shadow-amber-600/20 rounded-xl py-2.5 font-medium px-7 bg-gradient-to-tl from-amber-500 to-amber-700 text-white  hover:opacity-80 transition duration-200">
-                               Invite
+                               Source
                             </a>
                </Link>
                     </div>
