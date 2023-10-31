a5b6895bce95491cc15228d7e0aaacf53fcd8cb4
benjisqt <128649752+benjisqt@users.noreply.github.com>
2023-10-31 03:09:50 +0000

    ⚙️ change: removed "bot" text

--- components/Static/Header.jsx
+++ components/Static/Header.jsx
@@ -83,7 +83,7 @@ const Header = ({ $, NavItems }) => {
                                 width="48" height="48"
                             />
                             <p className="invisible md:visible text-xl text-white font-semibold">
-                              <a href="/"><span className="text-amber-400">Stylar</span>Bot</a>
+                              <a href="/"><span className="text-amber-400">Stylar</span></a>
                             </p>
                         </div>
                         <ul className="hidden lg:flex items-center space-x-4">
